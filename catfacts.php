4429e7d65e544d897576ddf1bbc525f36cfc03e1
Chad Doebelin <chad.doebelin@gmail.com>
2015-07-29 21:39:22 -0500

    removed some commented unused code

--- catfacts.php
+++ catfacts.php
@@ -1,16 +1,5 @@
 <?php
 
-function speechOut($words) { 
-	header('Content-type: application/json');
-
-	$open = '{"version": "1.0","sessionAttributes":"","response":{"outputSpeech":{"type": "PlainText","text": "';
-	$close = '"},"shouldEndSession": false}}';
-	$end = '"},"shouldEndSession": false}}';
-
-	echo $open;
-	 echo ($words);
-	echo $close;
-}
 
 function endSession($words) { 
 	header('Content-type: application/json');
@@ -26,10 +15,6 @@ function endSession($words) {
 
 $data = json_decode(file_get_contents('http://catfacts-api.appspot.com/api/facts?number=1'), TRUE);
 $text = print_r($data,true);
-//$ver = $data['session'][sessionId];
 $ver = $data['facts'][0];
-//speechOut($ver);
 endSession($ver);
-//echo('test');
-//echo($ver);
 ?>
